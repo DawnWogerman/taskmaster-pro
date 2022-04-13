3a377c4c1da2de39ce692e9ab5f60d16c45cbe87
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-04-13 13:01:48 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ var auditTask = function(taskEl){
   else if (Math.abs(moment().diff(time, "days")) <=2){
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 };
 
 var createTask = function(taskText, taskDate, taskList) {
@@ -171,7 +172,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -209,17 +210,19 @@ $(".card .list-group").sortable({
     scroll: false,
     tolerance: "pointer",
     helper: "clone",
-    activate: function(event){
-        console.log("activate", this);
+    activate: function(event, ui){
+        $(this).addClass("dropover");
+        $(".bottom-trash").addClass("bottom-trash-drag");
     },
-    deactivate: function(event){
-        console.log("deactivate", this);
+    deactivate: function(event, ui){
+        $(this).removeClass("dropover");
+        $(".bottom-trash").removeClass("bottom-trash-drag");
     },
     over: function(event){
-        console.log("over", event.target);
+        $(event.target).addClass("dropover-active");
     },
     out: function(event){
-        console.log("out", event.target);
+        $(event.target).removeClass("dropover-active");
     },
     update: function(event){
         //array to store the task data in
@@ -257,14 +260,15 @@ $("#trash").droppable({
     accept: ".card .list-group-item",
     tolerance: "touch",
     drop: function(event, ui){
-        console.log("drop");
         ui.draggable.remove();
+        $(".bottom-trash").removeClass("bottom-trash-active");
     },
     over: function(event, ui){
-        console.log("over");
+        console.log(ui);
+        $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function(event, ui){
-        console.log("out");
+        $(".bottom-trash").removeClass("bottom-trash-active");
     }
 });
 
@@ -272,3 +276,8 @@ $("#modalDueDate").datepicker({
     minDate:1
 });
 
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  });
+}, (1000*60) * 30);
